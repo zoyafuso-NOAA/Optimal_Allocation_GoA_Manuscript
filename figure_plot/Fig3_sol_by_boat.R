1ee8c2bfa13bd9770f68e8b29364d549b9251720
zoyafuso--NOAA <zack.oyafuso@noaa.gov>
2020-12-18 12:43:16 -0800

    removed maps w/o stations

--- figure_plot/Fig3_sol_by_boat.R
+++ figure_plot/Fig3_sol_by_boat.R
@@ -8,27 +8,25 @@
 ###############################################################################
 rm(list = ls())
 
+############################
+## Set up directories
+#############################
+which_machine <- c("Zack_MAC" = 1, "Zack_PC" = 2)[2]
+
+github_dir <- paste0(c("/Users/zackoyafuso/Documents/", 
+                       "C:/Users/Zack Oyafuso/Documents/")[which_machine], 
+                     "GitHub/Optimal_Allocation_GoA_Manuscript/model_6g/")
+
+figure_dir <- paste0(c("/Users/zackoyafuso/Google Drive/", 
+                       "C:/Users/Zack Oyafuso/Google Drive/")[which_machine],
+                     "MS_Optimizations/Manuscript Drafts/figure_plot/")
+
 ##################################################
 ####   Import required packages
 ##################################################
 library(sp)
 library(raster)
 library(RColorBrewer)
-
-############################
-## Set up directories
-#############################
-which_machine <- c("Zack_MAC" = 1, "Zack_PC" = 2)[1]
-
-VAST_model <- "6g"
-github_dir <- paste0(c("/Users/zackoyafuso/Documents/", 
-                       "C:/Users/Zack Oyafuso/Documents/")[which_machine], 
-                     "GitHub/Optimal_Allocation_GoA_Manuscript/model_",
-                     VAST_model, "/")
-
-figure_dir <- paste0(c("/Users/zackoyafuso/Google Drive/", 
-                       "C:/Users/Zack Oyafuso/Google Drive/")[which_machine],
-                     "MS_Optimizations/figure_plot/")
 
 ########################
 ## Load Data
@@ -44,126 +42,112 @@ load(paste0(github_dir, "Spatiotemporal_Optimization_SppSpecificCV/",
 samples <- c(820, 550, 280)
 stratas <- c(5, 10, 15)
 yrange <- diff(range(Extrapolation_depths[, "N_km"]))
-plot_random_sample <- F
 
 {
-  png(file = paste0(figure_dir, "Fig3_sol_by_boat",
-                    ifelse(plot_random_sample == T, "_withsamples", ""),
-                    ".png"),
-      width = 130,
-      height = 180,
+  png(file = paste0(figure_dir, "Fig3_sol_by_boat2_withsamples.png"),
+      width = 190,
+      height = 150,
       units = "mm",
       res = 1000)
   
   ####################################
   ## Plot Settings
   ####################################
-  par(mfcol = c(3, 2), 
+  par(mfrow = c(2, 2), 
       mar = c(0, 0, 2, 0),
       oma = c(0.25, 0.25, 1.5, 0.25) )
   
-  for (plot_random_sample in c(F, T)) {
-    for (istrata in stratas) {
+  
+  for (istrata in stratas) {
+    
+    ##################################
+    ## Empty Plot
+    ##################################
+    plot(1, 
+         type = "n", 
+         axes = F, 
+         ann = F,
+         asp = 1,
+         xlim = range(Extrapolation_depths[, c("E_km")]),
+         ylim = c(min(Extrapolation_depths[, c("N_km")]) - 1.2 * yrange,
+                  max(Extrapolation_depths[, c("N_km")])))
+    
+    mtext(side = 3, 
+          paste(istrata, "Strata"), 
+          font = 2) 
+    
+    offset <- 0
+    for(isample in samples){
+      sub_settings <- subset(settings, 
+                             strata == istrata)
       
-      ##################################
-      ## Empty Plot
-      ##################################
-      plot(1, 
-           type = "n", 
-           axes = F, 
-           ann = F,
-           xlim = range(Extrapolation_depths[, c("E_km")]),
-           ylim = c(min(Extrapolation_depths[, c("N_km")]) - 1.5 * yrange,
-                    max(Extrapolation_depths[, c("N_km")])))
+      isol <- sub_settings$id[which.min(abs(sub_settings$n - isample))]
       
-      mtext(side = 3, 
-            paste(istrata, "Strata"), 
-            font = 2) 
+      #Plot Solution
+      goa <- sp::SpatialPointsDataFrame(
+        coords = Extrapolation_depths[, c("E_km", "N_km")],
+        data = data.frame(stratum = res_df[, isol + 1]) )
+      goa_ras <- raster::raster(x = goa, 
+                                resolution = 5)
+      goa_ras <- raster::rasterize(x = goa, 
+                                   y = goa_ras, 
+                                   field = "stratum")
       
-      offset <- 0
-      for(isample in samples){
-        sub_settings <- subset(settings, 
-                               strata == istrata)
-        
-        isol <- sub_settings$id[which.min(abs(sub_settings$n - isample))]
-        
-        #Plot Solution
-        goa <- sp::SpatialPointsDataFrame(
-          coords = Extrapolation_depths[, c("E_km", "N_km")],
-          data = data.frame(stratum = res_df[, isol+1]) )
-        goa_ras <- raster::raster(x = goa, 
-                                  resolution = 5)
-        goa_ras <- raster::rasterize(x = goa, 
-                                     y = goa_ras, 
-                                     field = "stratum")
-        
-        goa_ras <- raster::shift(goa_ras, 
-                                 dy = offset)
-        
-        
-        if(istrata != 15) {
-          sol_col = brewer.pal(n = istrata, 
-                               name = "Paired")
-        }
-        
-        if(istrata == 15) {
-          sol_col = c(brewer.pal(n = 12, 
-                                 name = "Paired"),
-                      rev(brewer.pal(n = 11, 
-                                     name = "Spectral"))[11-istrata] )
-        }
-        
-        #Plot Solution
-        image(goa_ras, 
-              add = T,
-              asp = 1, 
-              axes = F, 
-              col = sol_col)
-        # box()
-        
-        #Plot sample size label
-        xrange <- diff(goa_ras@extent[1:2])
-        yrange <- diff(goa_ras@extent[3:4])
-        text(x = goa_ras@extent[1] + xrange * 0.70, 
-             y = goa_ras@extent[3] + yrange * 0.55,
-             labels = paste0("n = ", isample, "\n"))
-        
-        #If Strata == 5, plot subtitle for Simulated Survey
-       if (istrata == 5)  mtext(side = 3, 
-                                text = ifelse(plot_random_sample, 
-                                              "Strata with Simulated Survey",
-                                              "Strata Only"), 
-                                line = 2,
-                                font = 2)
-        
-        if(plot_random_sample) {
-          #Sample based on the stratification allocations
-          sample_vec <- c()
-          for (i in 1:istrata ){
-            available_cells <- which(res_df[, isol+1] == i)
-            
-            if(length(available_cells) > 0){
-              sample_cells <- sample(x = available_cells, 
-                                     size = strata_list[[isol]]$Allocation[i], 
-                                     replace = F)
-              sample_vec <- c(sample_vec, sample_cells)
-            }
-            
-          }
-          
-          #Organize sample set and total number of samples
-          sample_vec <- sort(sample_vec)
-          sample_pts <- Extrapolation_depths[sample_vec, c("E_km", "N_km")]
-          sample_pts[, "N_km"] <- sample_pts[, "N_km"] + offset
-          
-          points(sample_pts, 
-                 pch = 16, 
-                 cex = 0.2)
-        }
-        
-        #Update offset
-        offset <- offset - yrange*.7
+      goa_ras <- raster::shift(goa_ras, 
+                               dy = offset)
+      
+      
+      if(istrata != 15) {
+        sol_col = brewer.pal(n = istrata, 
+                             name = "Paired")
       }
+      
+      if(istrata == 15) {
+        sol_col = c(brewer.pal(n = 12, 
+                               name = "Paired"),
+                    rev(brewer.pal(n = 11, 
+                                   name = "Spectral"))[11-istrata] )
+      }
+      
+      #Plot Solution
+      image(goa_ras, 
+            add = T,
+            asp = 1, 
+            axes = F, 
+            col = sol_col)
+      
+      #Plot sample size label
+      xrange <- diff(goa_ras@extent[1:2])
+      yrange <- diff(goa_ras@extent[3:4])
+      text(x = goa_ras@extent[1] + xrange * 0.70, 
+           y = goa_ras@extent[3] + yrange * 0.65,
+           labels = paste0("n = ", isample, "\n"))
+      
+      #Sample based on the stratification allocations
+      sample_vec <- c()
+      for (i in 1:istrata ){
+        available_cells <- which(res_df[, isol + 1] == i)
+        
+        if(length(available_cells) > 0){
+          sample_cells <- sample(x = available_cells, 
+                                 size = strata_list[[isol]]$Allocation[i], 
+                                 replace = F)
+          sample_vec <- c(sample_vec, sample_cells)
+        }
+        
+      }
+      
+      #Organize sample set and total number of samples
+      sample_vec <- sort(sample_vec)
+      sample_pts <- Extrapolation_depths[sample_vec, c("E_km", "N_km")]
+      sample_pts[, "N_km"] <- sample_pts[, "N_km"] + offset
+      
+      points(sample_pts, 
+             pch = 16, 
+             cex = 0.25)
+      
+      #Update offset
+      offset <- offset - yrange*.6
     }
   }
   dev.off()
